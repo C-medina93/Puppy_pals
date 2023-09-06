f8bde70606c766adba1a06d569d82ad4696d62a4
Christopher <Chris.Medina93@gmail.com>
2023-09-06 07:29:09 -0700

    before render

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import { puppyList } from './data'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 console.log(puppies);
   return (
     <>
     {
       puppies.map((puppy)=>{
-        return <p key = {puppy.id}>{puppy.name}</p>
+        return <p onClick={()=>{setFeatPupId(puppy.id)}} key = {puppy.id}>{puppy.name}</p>
       })
-    }
 
+    }
+    { featPupId && <p>{featPupId} </p>}
+   
     </>
   )
 }
